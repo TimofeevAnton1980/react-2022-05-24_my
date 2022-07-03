a849a3fbb1b27170ba71c055cd817e98a2d449f2
antontimofeev <Maestroat@yandex.ru>
2022-07-03 23:20:08 +0300

    lesson_8_9

--- src/containers/MenuReviewContainer/MenuReviewContainer.jsx
+++ src/containers/MenuReviewContainer/MenuReviewContainer.jsx
@@ -1,7 +1,4 @@
-import { Tabs } from "../../components/Tabs/Tabs";
 import React from "react";
-import { useSelector } from "react-redux";
-import { selectRestaurants } from "../../store/restaurant/selectors";
 import { useParams } from "react-router-dom";
 import { TabsMenuReview } from "../../components/TabsMenuReview/TabsMenuReview";
 
